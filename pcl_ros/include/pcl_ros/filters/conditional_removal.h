4f86408dd5885fe248281ffa020ced4ee7881f1d
Evan Flynn <evanflynn.msu@gmail.com>
2021-08-05 10:25:49 -0700

    start conditionalremoval filter

---
+++ pcl_ros/include/pcl_ros/filters/conditional_removal.h
@@ -0,0 +1,108 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef PCL_ROS_FILTERS_CONDITIONAL_REMOVAL_H_
+#define PCL_ROS_FILTERS_CONDITIONAL_REMOVAL_H_
+
+#include <string>
+
+// PCL includes
+#include <pcl/filters/conditional_removal.h>
+#include "pcl_ros/filters/filter.h"
+
+namespace pcl_ros
+{
+
+  template <typename T>
+  struct FieldComparison {
+    std::string fieldName;
+    pcl::ComparisonOps::CompareOp compareOp;
+    T value;
+  }
+
+  /** \brief @b ConditionalRemoval uses the base Filter class methods to pass through all data that satisfies the user given
+    * constraints.
+    * \author Evan Flynn
+    */
+  class ConditionalRemoval : public Filter
+  {
+    protected:
+      /** \brief Pointer to a dynamic reconfigure service. */
+      std::shared_ptr<dynamic_reconfigure::Server<pcl_ros::FilterConfig>> m_dynamicParamServer;
+
+      /** \brief Call the actual filter.
+        * \param input the input point cloud dataset
+        * \param indices the input set of indices to use from \a input
+        * \param output the resultant filtered dataset
+        */
+      inline void
+      filter (const PointCloud2::ConstPtr &input, const IndicesPtr &indices, 
+              PointCloud2 &output)
+      {
+        pcl::PCLPointCloud2::Ptr pcl_input(new pcl::PCLPointCloud2);
+        pcl_conversions::toPCL (*(input), *(pcl_input));
+        m_removeFilter.setInputCloud (pcl_input);
+        m_removeFilter.setIndices (indices);
+        pcl::PCLPointCloud2 pcl_output;
+        m_removeFilter.filter (pcl_output);
+        pcl_conversions::moveFromPCL(pcl_output, output);
+      }
+
+      /** \brief Child initialization routine.
+        * \param nh ROS node handle
+        * \param has_service set to true if the child has a Dynamic Reconfigure service
+        */
+      bool 
+      child_init (ros::NodeHandle &nh, bool &has_service);
+      
+      /** \brief Dynamic reconfigure service callback.
+        * \param config the dynamic reconfigure FilterConfig object
+        * \param level the dynamic reconfigure level
+        */
+      void 
+      config_callback (pcl_ros::FilterConfig &config, uint32_t level);
+
+    private:
+      /** \brief The PCL filter implementation used. */
+      pcl::ConditionalRemoval<pcl::PCLPointCloud2> m_removeFilter;
+
+      std::vector<pcl_ros::FieldComparison> m_comparisons;
+      
+    public:
+      EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  };
+}
+
+#endif  //#ifndef PCL_ROS_FILTERS_CONDITIONAL_REMOVAL_H_
